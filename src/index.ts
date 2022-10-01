f93c5d90a47d2ddaa2ec8b070a7d10d65fa2a2e3
Muhd Raihan <muhdraihan1140@gmail.com>
2022-10-01 17:28:00 +0800

    added main program index

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import { alchemyData } from "./alchemy/alchemy-data";
+import { mix } from "./alchemy/alchemy-utils";
+
+function main() {
+  if (process.argv.length == 4) {
+    const item1: string = process.argv[2];
+    const item2: string = process.argv[3];
+    const mixed = mix(item1, item2);
+    if (mixed != null) {
+      console.log(
+        `${alchemyData[item1].prettyPrint} and ${alchemyData[item2].prettyPrint} makes ${alchemyData[mixed].prettyPrint}.`
+      );
+    } else {
+      console.log("No such mixture.");
+    }
+  }
+}
+main();
